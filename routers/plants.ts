445ff3c751d6b2b7cc079439139b635d30c572f1
JarosławLeszko <56537814+JaroslawLeszko@users.noreply.github.com>
2022-09-29 14:47:19 +0200

    Update plants.ts

--- routers/plants.ts
+++ routers/plants.ts
@@ -50,7 +50,7 @@ plantsRouter
 
     .get('/:image', async (req, res) => {
         // const plantPhoto = await PlantRecord.getOne(req.params.image);
-        res.sendFile(path.resolve((path.resolve(__dirname, req.params.image))));
+        res.sendFile(path.resolve(__dirname, req.params.image));
     })
 
     .post('/', async (req, res) => {
